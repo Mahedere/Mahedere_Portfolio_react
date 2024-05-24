e4bad939b9c97569cd0068617a68c470aef8dc48
Mahedere <mahederetebebe98@gmail.com>
2024-05-24 14:11:32 -0700

    Header completed

---
+++ react-mahedere-portfolio/src/components/Header/HeaderSocials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://www.linkedin.com/in/mahedere-tebebe/" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
--- react-mahedere-portfolio/src/components/Header/header.jsx
+++ react-mahedere-portfolio/src/components/Header/header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import MAHI from '../../assets/mahi.png'
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Mahedere Tebebe</h1>
+        <h5 className="text-light">Web Developer</h5>
+        <CTA></CTA>
+        <div className="me">
+          <img src={MAHI} alt="me" />
+        </div>
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
