d5261b6f4aa259fdea0d721cfedd61bbed4dbc52
Mahedere <mahederetebebe98@gmail.com>
2024-05-27 23:01:39 +0300

    services section removed

--- react-mahedere-portfolio/src/App.jsx
+++ react-mahedere-portfolio/src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header/header'
 import Nav from './components/Nav/nav'
 import About from './components/About/about'
 import Experience from './components/Experience/experience'
-import Services from './components/Services/services'
 import Portfolio from './components/Portfolio/portfolio'
 import Testimonials from './components/Testimonials/testimonials'
 import Contact from './components/Contact/contact'
@@ -15,7 +14,6 @@ const App = () => {
     <Nav />
     <About />
     <Experience />
-    <Services />
     <Portfolio />
     <Testimonials />
     <Contact />
