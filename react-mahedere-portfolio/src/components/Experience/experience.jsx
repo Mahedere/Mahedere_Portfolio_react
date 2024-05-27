5d032312013b3a4457621a0444cf16fd837473fc
Mahedere <mahederetebebe98@gmail.com>
2024-05-27 22:58:44 +0300

    experience section completed

--- react-mahedere-portfolio/src/components/Experience/experience.jsx
+++ react-mahedere-portfolio/src/components/Experience/experience.jsx
@@ -6,14 +6,50 @@ const experience = () => {
     <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
-      <div className="container experience_frontend">
-        <div className="experience_fronend">
-          <h3>Frontend Development</h3>
+      <div className="container experience">
+        <div className="experience_frontend">
           <div className="experience_content">
             <article className="experience_details">
               <BsFillCheckCircleFill/>
+              <div>
               <h4>Html</h4>
               <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill/>
+              <div>
+              <h4>Css</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill/>
+              <div>
+              <h4>Javascript</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill/>
+              <div>
+              <h4>ReactJs</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill/>
+              <div>
+              <h4>TailwindCss</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillCheckCircleFill/>
+              <div>
+              <h4>NodeJs</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
             </article>
           </div>
         </div>
